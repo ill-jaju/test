74544e0eee5459dcd293d3b374b1a04a69c2e635
jamie-jessi <jamiejudi@gmail.com>
2017-07-01 17:50:46 -0500

    ajax test

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
-$('#addShow').on("click", function() { //event handler for submit button
+$(document).ready(function() {
+    $('#addShow').on("click", function(e) { //event handler for submit button
         event.preventDefault(); //prevents refreshing
-        var show = $('#titleSearch').val().trim(); //takes user input from from
-        var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=50c9867e013d532a54d305162ee29e35&query=" + show;
 
+        var show = $('#titleSearch').val().trim(); //takes user input from search
+        var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=50c9867e013d532a54d305162ee29e35&query=" + show;
         $.ajax({ //AJAX call for specific show being clicked
             url: queryURL,
             method: "GET"
@@ -11,34 +12,72 @@ $('#addShow').on("click", function() { //event handler for submit button
 
             $('#moviesHere').empty();
 
-            var results = response.results;
-            for (var i = 0; i < results.length; i++) {
-                var movieBox = $('<div>');
-                movieBox.addClass('col-xs-12 col-md-6');
+            var results = response.results; //ajax results into variable
 
-                var posterBox = $('<div>');
-                posterBox.addClass('col-xs-12 col-md-3');
+            for (var i = 0; i < results.length; i++) {
+
+                var str = results[i].title; //lower case search term, replace spaces with dashes
+                str = str.replace(/\s+/g, '-').toLowerCase();
+
+                var yearFull = results[i].release_date; //takes release date from themoviedb 
+                var year = yearFull.substring(0, 4); //extracts the year
+                console.log(year);
+
+                var movieBox = $('<div>'); //creates div for poster + info
+                movieBox.addClass('col-xs-12 col-md-6 movieBox');
+                movieBox.attr('data-name', str);
+
+                var posterBox = $('<div>'); //creates div for poster
+                posterBox.addClass('col-xs-12 col-md-4');
 
                 var img = $('<img>');
+                img.addClass('img-responsive');
                 img.attr("src", "http://image.tmdb.org/t/p/w185//" + results[i].poster_path);
                 posterBox.append(img);
 
-                var infoBox = $('<div>');
-                infoBox.addClass('col-xs-12 col-md-6 infoBox');                
+                var infoBox = $('<div>'); //creates div for information
+                infoBox.addClass('col-xs-12 col-md-8');
 
-                var information = $('<h5>').html("<tr> <td>" + results[i].title + " </td></tr> " + "<tr><td>" + results[i].overview + " </td></tr> ");
+                var information = $('<h5>').html( results[i].title + "<br><br>" + results[i].overview);
                 infoBox.append(information);
 
-                var reviewBox = $('<div>');
-                reviewBox.addClass('col-xs-12 col-md-3');
+                var imdbRatings, rottenRatings, metaRatings;
+                var queryURL = "http://www.omdbapi.com/?t=" + str + "&y=" + year + "&apikey=40e9cece"; //movie + year query
+                
+                $.ajax({ //ajax call to omdb for reviews 
+                    url: queryURL,
+                    method: "GET"
+                }).done(function(response) {
 
-                var reviews =  $('<h5>').html("The Movie DB - " + results[i].vote_average);
-                reviewBox.append(reviews);
+                    imdbHundred = response.Ratings[0].Value;
+                    imdbNums = imdbHundred.split('/');
+                    imdbRatings = parseFloat(imdbNums[0]);
 
-                movieBox.append(posterBox, infoBox, reviewBox);
+                    rottenRatings = (parseFloat(response.Ratings[1].Value) / 10);
+
+                    metaHundred = (response.Ratings[2].Value);
+                    metaNums = metaHundred.split('/');
+                    metaRatings = parseFloat(metaNums[0] / 10);
+
+                    //rating results
+                    console.log(imdbRatings);
+                    console.log(rottenRatings);
+                    console.log(metaRatings);
+
+                    var reelRatingAdd = (imdbRatings + rottenRatings + metaRatings);
+                    var reelRating = (reelRatingAdd / 3);
+                    console.log("Reel Rating is " + Math.round(reelRating * 10) / 10);
+
+                    // var totalRatings = $('<h5>').html( imdbRatings );
+                    // infoBox.append(totalRatings);
+
+                });
+
+                movieBox.append(posterBox, infoBox);
 
                 $('#moviesHere').append(movieBox);
-           }
-        });
 
+            }
+        });
     });
+});
