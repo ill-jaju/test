fb9ace1fbbcf7b1b2790186ef7edfe9896e766f3
jamie-jessi <jamiejudi@gmail.com>
2017-07-03 17:57:30 -0500

    modal

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     $('#addShow').on("click", function(e) { //event handler for submit button
         event.preventDefault(); //prevents refreshing
 
@@ -24,56 +25,21 @@ $(document).ready(function() {
                 console.log(year);
 
                 var movieBox = $('<div>'); //creates div for poster + info
-                movieBox.addClass('col-xs-12 col-md-6 movieBox');
-                movieBox.attr('data-name', str);
+                movieBox.addClass('col-xs-6 col-lg-4 movieBox');
 
                 var posterBox = $('<div>'); //creates div for poster
-                posterBox.addClass('col-xs-12 col-md-4');
+                posterBox.addClass('col-xs-12');
 
                 var img = $('<img>');
                 img.addClass('img-responsive');
-                img.attr("src", "https://image.tmdb.org/t/p/w185//" + results[i].poster_path);
-                posterBox.append(img);
+                img.attr("src", "https://image.tmdb.org/t/p/w342//" + results[i].poster_path);
+                img.attr('data-name', str);
+                img.attr('data-id', results[i].id);
+                img.attr('data-ratings', str + "&y=" + year);
+                var information = $('<h5>').html(results[i].title + " ("+ year +")");
+                posterBox.append(img, information);
 
-                var infoBox = $('<div>'); //creates div for information
-                infoBox.addClass('col-xs-12 col-md-8');
-
-                var information = $('<h5>').html( results[i].title + "<br><br>" + results[i].overview);
-                infoBox.append(information);
-
-                var imdbRatings, rottenRatings, metaRatings;
-                var queryURL = "https://www.omdbapi.com/?t=" + str + "&y=" + year + "&apikey=40e9cece"; //movie + year query
-                
-                $.ajax({ //ajax call to omdb for reviews 
-                    url: queryURL,
-                    method: "GET"
-                }).done(function(response) {
-
-                    imdbHundred = response.Ratings[0].Value;
-                    imdbNums = imdbHundred.split('/');
-                    imdbRatings = parseFloat(imdbNums[0]);
-
-                    rottenRatings = (parseFloat(response.Ratings[1].Value) / 10);
-
-                    metaHundred = (response.Ratings[2].Value);
-                    metaNums = metaHundred.split('/');
-                    metaRatings = parseFloat(metaNums[0] / 10);
-
-                    //rating results
-                    console.log(imdbRatings);
-                    console.log(rottenRatings);
-                    console.log(metaRatings);
-
-                    var reelRatingAdd = (imdbRatings + rottenRatings + metaRatings);
-                    var reelRating = (reelRatingAdd / 3);
-                    console.log("Reel Rating is " + Math.round(reelRating * 10) / 10);
-
-                    // var totalRatings = $('<h5>').html( imdbRatings );
-                    // infoBox.append(totalRatings);
-
-                });
-
-                movieBox.append(posterBox, infoBox);
+                movieBox.append(posterBox);
 
                 $('#moviesHere').append(movieBox);
 
